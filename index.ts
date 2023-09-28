7fca05c9cfd17e394a1427494e260a3883aa8f24
Dallin Christensen <christensen.dallin@gmail.com>
2023-09-27 22:19:18 -0700

    socket.io setup

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import books from './routes/api/books'
 import rooms from './routes/api/rooms'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { Server } from 'socket.io';
+import { insertLatestMessage } from './controllers/RoomController'
 
 connectDB()
 
@@ -23,9 +25,33 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
 app.use('/api/books', books)
 app.use('/api/rooms', rooms)
+
+
+const io = new Server(server, {
+  cors: "http://localhost:5173"
+} as any);
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('insertmessage', async (payload) => {
+    const { roomId, participantId, text } = payload
+    const updatedRoom = await insertLatestMessage(roomId, participantId, text)
+    io.emit('messageinserted', updatedRoom)
+
+  });
+});
+
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
