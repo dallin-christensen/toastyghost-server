8bd570dd1965fb1698b34eeae890af96e6ea887c
Dallin Christensen <christensen.dallin@gmail.com>
2023-10-07 17:54:22 -0700

    added full err to error response

--- middleware/errorHander.ts
+++ middleware/errorHander.ts
@@ -7,9 +7,9 @@ const errorHandler = (
     next: NextFunction
 ) => {
     if (err.message === 'room does not exist') {
-        res.status(404).send({ errors: [{ message: err.message }] })
+        res.status(404).send({ errors: [{ message: err.message, err }] })
     } else {
-        res.status(500).send({ errors: [{ message: 'Something went wrong' }] })
+        res.status(500).send({ errors: [{ message: 'Something went wrong', err }] })
     }
     next(err)
 }
