79f79472d485854fd196c3dd491f0e1ae7162f6c
Dallin Christensen <christensen.dallin@gmail.com>
2023-10-09 13:52:02 -0700

    cors revert pt 15

--- index.ts
+++ index.ts
@@ -21,8 +21,7 @@ connectDB()
 
 const app = express()
 
-// app.use(cors({ origin: 'https://chatterblight-client.vercel.app', credentials: true }))
-app.use(cors({ origin: "*", credentials: true }))
+app.use(cors({ origin: 'https://chatterblight-client.vercel.app', credentials: true }))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
