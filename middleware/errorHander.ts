a8de45f7b0ebf1bc11a2db9f992cc7d59b87b1f0
Dallin Christensen <christensen.dallin@gmail.com>
2023-10-08 23:33:40 -0700

    Revert "added full err to error response"
    This reverts commit 8bd570dd1965fb1698b34eeae890af96e6ea887c.

--- middleware/errorHander.ts
+++ middleware/errorHander.ts
@@ -7,9 +7,9 @@ const errorHandler = (
     next: NextFunction
 ) => {
     if (err.message === 'room does not exist') {
-        res.status(404).send({ errors: [{ message: err.message, err }] })
+        res.status(404).send({ errors: [{ message: err.message }] })
     } else {
-        res.status(500).send({ errors: [{ message: 'Something went wrong', err }] })
+        res.status(500).send({ errors: [{ message: 'Something went wrong' }] })
     }
     next(err)
 }
